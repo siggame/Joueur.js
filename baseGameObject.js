04f0419c2acd8a0bb2e789c5af7fdb995118bd8d
Jacob Fischer <jacob.t.fischer@gmail.com>
2015-09-03 15:48:34 -0500

    Adding Dossier data for auto doc generation.
    Also fixed a docstyle in baseGameObject

--- baseGameObject.js
+++ baseGameObject.js
@@ -1,6 +1,8 @@
 var Class = require("./utilities/class");
 
-// @class BaseGameObject: the base class that every game object within a game inherit from for Python manipulation that would be redundant via Creer
+/**
+ * @class BaseGameObject: the base class that every game object within a game inherit from for Python manipulation that would be redundant via Creer
+ */
 var BaseGameObject = Class({
     init: function() {
         // pass
