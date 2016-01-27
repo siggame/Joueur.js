de5b1e49ac2be19e5433c43821d782a436cb80a1
Jacob Fischer <jacob.t.fischer@gmail.com>
2016-01-27 14:38:45 -0600

    player one is index 0

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ parser.addArgument(['game'], {action: 'store', help: 'the name of the game you w
 parser.addArgument(['-s', '--server'], {action: 'store', dest: 'server', defaultValue: '127.0.0.1', help: 'the url to the server you want to connect to e.g. locahost:3000'});
 parser.addArgument(['-p', '--port'], {action: 'store', dest: 'port', defaultValue: 3000, help: 'the port to connect to on the server. Can be defined on the server arg via server:port'});
 parser.addArgument(['-n', '--name'], {action: 'store', dest: 'playerName', help: 'the name you want to use as your AI\'s player name'});
-parser.addArgument(['-i', '--index'], {action: 'store', dest: 'index', help: 'the player number you want to be, with 1 being the first player'});
+parser.addArgument(['-i', '--index'], {action: 'store', dest: 'index', help: 'the player number you want to be, with 0 being the first player'});
 parser.addArgument(['-w', '--password'], {action: 'store', dest: 'password', help: 'the password required for authentication on official servers'});
 parser.addArgument(['-r', '--session'], {action: 'store', dest: 'session', help: 'the requested game session you want to play on the server', defaultValue: '*'});
 parser.addArgument(['--gameSettings'], {action: 'store', dest: 'gameSettings', help: 'Any settings for the game server to force. Must be url parms formatted (key=value&otherKey=otherValue)'});
