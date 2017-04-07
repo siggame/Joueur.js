50f3ce4bc9585ae7965f5b2c02db642ee19e469f
Jacob Fischer <jacob.t.fischer@gmail.com>
2017-04-07 14:56:26 -0500

    Add BaseGameObject.toString and fix tiled function

--- joueur/baseGameObject.js
+++ joueur/baseGameObject.js
@@ -1,6 +1,10 @@
 /**
  * @class BaseGameObject: the base class that every game object within a game inherit from for Python manipulation that would be redundant via Creer
  */
-class BaseGameObject {}
+class BaseGameObject {
+  toString() {
+    return `${this.gameObjectName} #${this.id}`;
+  }
+}
 
 module.exports = BaseGameObject;
