6565a905167fac1ea3f6951d3157aa1528b8fc1f
JeffreyStrahm <jssfg8@mst.edu>
2018-09-07 14:17:28 -0500

    add  newtonian to joueur

---
+++ games/newtonian/machine.js
@@ -0,0 +1,182 @@
+// Machine: A machine on a tile.
+
+// DO NOT MODIFY THIS FILE
+// Never try to directly create an instance of this class, or modify its member variables.
+// Instead, you should only be reading its variables and calling its functions.
+
+const client = require(`${__basedir}/joueur/client`);
+const GameObject = require('./gameObject');
+
+//<<-- Creer-Merge: requires -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+// any additional requires you want can be required here safely between creer runs
+//<<-- /Creer-Merge: requires -->>
+
+/**
+ * A machine on a tile.
+ * @extends Newtonian.GameObject
+ * @memberof Newtonian
+ */
+class Machine extends GameObject {
+  /**
+   * Initializes a Machine with basic logic as provided by the Creer code generator.
+   * 
+   * Never use this directly. It is for internal Joueur use.
+   */
+  constructor(...args) {
+    super(...args);
+
+
+    // The following values should get overridden when delta states are merged, but we set them here as a reference for you to see what variables this class has.
+
+    // default values for private member values
+    this.input = 0;
+    this.oreType = '';
+    this.output = 0;
+    this.refineInput = 0;
+    this.refineOutput = 0;
+    this.refineTime = 0;
+    this.tile = null;
+    this.timeLeft = 0;
+    this.worked = 0;
+
+    //<<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+    // any additional init logic you want can go here
+    //<<-- /Creer-Merge: init -->>
+  }
+
+
+  // Member variables
+
+  /**
+   * The amount of ore that is in the machine. Cannot be higher than the refineInput value.
+   *
+   * @type {number}
+   */
+  get input() {
+    return client.gameManager.getMemberValue(this, 'input');
+  }
+
+  set input(value) {
+    client.gameManager.setMemberValue(this, 'input', value);
+  }
+
+
+  /**
+   * What type of ore the machine takes it, also determins the type of material it outputs.
+   *
+   * @type {string}
+   */
+  get oreType() {
+    return client.gameManager.getMemberValue(this, 'oreType');
+  }
+
+  set oreType(value) {
+    client.gameManager.setMemberValue(this, 'oreType', value);
+  }
+
+
+  /**
+   * The amount of material that is waiting to be collected in the machine.
+   *
+   * @type {number}
+   */
+  get output() {
+    return client.gameManager.getMemberValue(this, 'output');
+  }
+
+  set output(value) {
+    client.gameManager.setMemberValue(this, 'output', value);
+  }
+
+
+  /**
+   * The amount of ore that needs to be inputted into the machine.
+   *
+   * @type {number}
+   */
+  get refineInput() {
+    return client.gameManager.getMemberValue(this, 'refineInput');
+  }
+
+  set refineInput(value) {
+    client.gameManager.setMemberValue(this, 'refineInput', value);
+  }
+
+
+  /**
+   * The amount of material that out of the machine after running.
+   *
+   * @type {number}
+   */
+  get refineOutput() {
+    return client.gameManager.getMemberValue(this, 'refineOutput');
+  }
+
+  set refineOutput(value) {
+    client.gameManager.setMemberValue(this, 'refineOutput', value);
+  }
+
+
+  /**
+   * The amount of turns this machine takes to refine the ore.
+   *
+   * @type {number}
+   */
+  get refineTime() {
+    return client.gameManager.getMemberValue(this, 'refineTime');
+  }
+
+  set refineTime(value) {
+    client.gameManager.setMemberValue(this, 'refineTime', value);
+  }
+
+
+  /**
+   * The Tile this Machine is on.
+   *
+   * @type {Newtonian.Tile}
+   */
+  get tile() {
+    return client.gameManager.getMemberValue(this, 'tile');
+  }
+
+  set tile(value) {
+    client.gameManager.setMemberValue(this, 'tile', value);
+  }
+
+
+  /**
+   * Time till the machine finishes running.
+   *
+   * @type {number}
+   */
+  get timeLeft() {
+    return client.gameManager.getMemberValue(this, 'timeLeft');
+  }
+
+  set timeLeft(value) {
+    client.gameManager.setMemberValue(this, 'timeLeft', value);
+  }
+
+
+  /**
+   * Tracks how many times this machine has been worked.
+   *
+   * @type {number}
+   */
+  get worked() {
+    return client.gameManager.getMemberValue(this, 'worked');
+  }
+
+  set worked(value) {
+    client.gameManager.setMemberValue(this, 'worked', value);
+  }
+
+
+
+  //<<-- Creer-Merge: functions -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+  // any additional functions you want to add to this class can be preserved here
+  //<<-- /Creer-Merge: functions -->>
+}
+
+module.exports = Machine;
---
+++ games/newtonian/unit.js
@@ -0,0 +1,296 @@
+// Unit: A unit in the game. May be a manager, intern, or physicist.
+
+// DO NOT MODIFY THIS FILE
+// Never try to directly create an instance of this class, or modify its member variables.
+// Instead, you should only be reading its variables and calling its functions.
+
+const client = require(`${__basedir}/joueur/client`);
+const GameObject = require('./gameObject');
+
+//<<-- Creer-Merge: requires -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+// any additional requires you want can be required here safely between creer runs
+//<<-- /Creer-Merge: requires -->>
+
+/**
+ * A unit in the game. May be a manager, intern, or physicist.
+ * @extends Newtonian.GameObject
+ * @memberof Newtonian
+ */
+class Unit extends GameObject {
+  /**
+   * Initializes a Unit with basic logic as provided by the Creer code generator.
+   * 
+   * Never use this directly. It is for internal Joueur use.
+   */
+  constructor(...args) {
+    super(...args);
+
+
+    // The following values should get overridden when delta states are merged, but we set them here as a reference for you to see what variables this class has.
+
+    // default values for private member values
+    this.acted = false;
+    this.blueium = 0;
+    this.blueiumOre = 0;
+    this.health = 0;
+    this.job = null;
+    this.moves = 0;
+    this.owner = null;
+    this.redium = 0;
+    this.rediumOre = 0;
+    this.stunImmune = 0;
+    this.stunTime = 0;
+    this.tile = null;
+
+    //<<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+    // any additional init logic you want can go here
+    //<<-- /Creer-Merge: init -->>
+  }
+
+
+  // Member variables
+
+  /**
+   * Whether this Unit has performed its action this turn.
+   *
+   * @type {boolean}
+   */
+  get acted() {
+    return client.gameManager.getMemberValue(this, 'acted');
+  }
+
+  set acted(value) {
+    client.gameManager.setMemberValue(this, 'acted', value);
+  }
+
+
+  /**
+   * The amount of blueium carried by this unit.
+   *
+   * @type {number}
+   */
+  get blueium() {
+    return client.gameManager.getMemberValue(this, 'blueium');
+  }
+
+  set blueium(value) {
+    client.gameManager.setMemberValue(this, 'blueium', value);
+  }
+
+
+  /**
+   * The amount of blueium ore carried by this unit.
+   *
+   * @type {number}
+   */
+  get blueiumOre() {
+    return client.gameManager.getMemberValue(this, 'blueiumOre');
+  }
+
+  set blueiumOre(value) {
+    client.gameManager.setMemberValue(this, 'blueiumOre', value);
+  }
+
+
+  /**
+   * If a ship is on this Tile, how much health it has remaining.
+   *
+   * @type {number}
+   */
+  get health() {
+    return client.gameManager.getMemberValue(this, 'health');
+  }
+
+  set health(value) {
+    client.gameManager.setMemberValue(this, 'health', value);
+  }
+
+
+  /**
+   * The Job this Unit does.
+   *
+   * @type {Newtonian.Job}
+   */
+  get job() {
+    return client.gameManager.getMemberValue(this, 'job');
+  }
+
+  set job(value) {
+    client.gameManager.setMemberValue(this, 'job', value);
+  }
+
+
+  /**
+   * How many more times this Unit may move this turn.
+   *
+   * @type {number}
+   */
+  get moves() {
+    return client.gameManager.getMemberValue(this, 'moves');
+  }
+
+  set moves(value) {
+    client.gameManager.setMemberValue(this, 'moves', value);
+  }
+
+
+  /**
+   * The Player that owns and can control this Unit.
+   *
+   * @type {Newtonian.Player}
+   */
+  get owner() {
+    return client.gameManager.getMemberValue(this, 'owner');
+  }
+
+  set owner(value) {
+    client.gameManager.setMemberValue(this, 'owner', value);
+  }
+
+
+  /**
+   * The amount of redium carried by this unit.
+   *
+   * @type {number}
+   */
+  get redium() {
+    return client.gameManager.getMemberValue(this, 'redium');
+  }
+
+  set redium(value) {
+    client.gameManager.setMemberValue(this, 'redium', value);
+  }
+
+
+  /**
+   * The amount of redium ore carried by this unit.
+   *
+   * @type {number}
+   */
+  get rediumOre() {
+    return client.gameManager.getMemberValue(this, 'rediumOre');
+  }
+
+  set rediumOre(value) {
+    client.gameManager.setMemberValue(this, 'rediumOre', value);
+  }
+
+
+  /**
+   * Duration of stun immunity.
+   *
+   * @type {number}
+   */
+  get stunImmune() {
+    return client.gameManager.getMemberValue(this, 'stunImmune');
+  }
+
+  set stunImmune(value) {
+    client.gameManager.setMemberValue(this, 'stunImmune', value);
+  }
+
+
+  /**
+   * Duration the unit is stunned.
+   *
+   * @type {number}
+   */
+  get stunTime() {
+    return client.gameManager.getMemberValue(this, 'stunTime');
+  }
+
+  set stunTime(value) {
+    client.gameManager.setMemberValue(this, 'stunTime', value);
+  }
+
+
+  /**
+   * The Tile this Unit is on.
+   *
+   * @type {Newtonian.Tile}
+   */
+  get tile() {
+    return client.gameManager.getMemberValue(this, 'tile');
+  }
+
+  set tile(value) {
+    client.gameManager.setMemberValue(this, 'tile', value);
+  }
+
+
+
+  /**
+   * Makes the unit do something to a machine on its tile. Interns sabotage, physicists run, and managers protect.
+   *
+   * @param {Newtonian.Tile} tile - The tile the unit acts on.
+   * @returns {boolean} - True if successfully acted, false otherwise.
+   */
+  act(tile) {
+    return client.runOnServer(this, 'act', {
+      tile: tile,
+    });
+  }
+
+
+  /**
+   * Attacks a unit on a ajacent tile.
+   *
+   * @param {Newtonian.Tile} tile - The Tile to attack.
+   * @returns {boolean} - True if successfully attacked, false otherwise.
+   */
+  attack(tile) {
+    return client.runOnServer(this, 'attack', {
+      tile: tile,
+    });
+  }
+
+
+  /**
+   * Drops material at the units feat
+   *
+   * @param {number} amount - The amount of materials to dropped. Amounts <= 0 will drop all the materials on the Unit.
+   * @param {string} material - The material the unit will drop.
+   * @returns {boolean} - True if successfully deposited, false otherwise.
+   */
+  drop(amount, material) {
+    return client.runOnServer(this, 'drop', {
+      amount: amount,
+      material: material,
+    });
+  }
+
+
+  /**
+   * Moves this Unit from its current Tile to an adjacent Tile.
+   *
+   * @param {Newtonian.Tile} tile - The Tile this Unit should move to.
+   * @returns {boolean} - True if it moved, false otherwise.
+   */
+  move(tile) {
+    return client.runOnServer(this, 'move', {
+      tile: tile,
+    });
+  }
+
+
+  /**
+   * Picks up material at the units feat
+   *
+   * @param {number} amount - The amount of materials to pick up. Amounts <= 0 will pick up all the materials on the Unit.
+   * @param {string} material - The material the unit will pick up.
+   * @returns {boolean} - True if successfully deposited, false otherwise.
+   */
+  pickup(amount, material) {
+    return client.runOnServer(this, 'pickup', {
+      amount: amount,
+      material: material,
+    });
+  }
+
+
+  //<<-- Creer-Merge: functions -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+  // any additional functions you want to add to this class can be preserved here
+  //<<-- /Creer-Merge: functions -->>
+}
+
+module.exports = Unit;
