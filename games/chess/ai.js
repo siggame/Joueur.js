fbf82eb386fff60efc46d845afcbe1406dfd8fda
Jacob Fischer <jacob.t.fischer@gmail.com>
2019-02-02 21:24:41 -0600

    Add logic to always play correctly

--- games/chess/ai.js
+++ games/chess/ai.js
@@ -1,46 +1,8 @@
-// This is where you build your AI for the Chess game.
+// This AI uses the Chess.js library to show all valid moves, then makes one at random.
+// Use it to test your Chess AIs, as this one can never fail to play correctly.
 
 const BaseAI = require(`${__basedir}/joueur/baseAI`);
-
-/**
- * Pretty formats an FEN string to a human readable string.
- *
- * For more information on FEN (Forsyth-Edwards Notation) strings see:
- * https://wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
- */
-function prettyFEN(fen, us) {
-  // split the FEN string up to help parse it
-  const split = fen.split(' ');
-  const first = split[0]; // the first part is always the board locations
-
-  const sideToMove = split[1]; // always the second part for side to move
-  const usOrThem = sideToMove === us[0] ? 'us'  : 'them';
-
-  const fullmove = split[5]; // always the sixth part for the full move
-
-  const lines = first.split('/');
-  const strings = [`Move: ${fullmove}\nSide to move: ${sideToMove} (${usOrThem})\n   +-----------------+`];
-
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    strings.push(`\n ${8 - i} |`);
-    for (const char of line) {
-      const charAsNumber = Number(char);
-      if (isNaN(charAsNumber)) {
-        strings.push(` ${char}`);
-      }
-      else { // it is a number, so that many blank lines
-        for (let j = 0; j < charAsNumber; j++) {
-          strings.push(' .');
-        }
-      }
-    }
-    strings.push(' |');
-  }
-  strings.push('\n   +-----------------+\n     a b c d e f g h\n');
-
-  return strings.join('');
-}
+const Chess = require('chess.js').Chess;
 
 /**
  * This is the class to play the Chess game. This is where you should build your AI.
@@ -65,22 +27,31 @@ class AI extends BaseAI {
    * @returns {string} - The name of your Player.
    */
   getName() {
-    // TODO: Rename your player here!
-    return 'Chess JavaScript Player';
+    return 'Chess Valid Tester';
   }
 
   /**
    * This is called once the game starts and your AI knows its playerID and game. You can initialize your AI here.
    */
   start() {
-    // pass
+    this.chess = new Chess();
+    this.printedEnemyMoves = false;
   }
 
   /**
    * This is called every time the game's state updates, so if you are tracking anything you can update it here.
    */
   gameUpdated() {
-    // pass
+    if (!this.game.fen || !this.player.color || this.printedEnemyMoves) {
+      return;
+    }
+
+    this.chess.load(this.game.fen);
+
+    if (this.chess.turn() !== this.player.color[0]) {
+      this.printChessInfo(`enemy's`);
+      this.printedEnemyMoves = true;
+    }
   }
 
   /**
@@ -100,12 +71,28 @@ class AI extends BaseAI {
    * @returns {string} - A string in Standard Algebraic Notation (SAN) for the move you want to make. If the move is invalid or not properly formatted you will lose the game.
    */
   makeMove() {
-    console.log(prettyFEN(this.game.fen, this.player.color));
+    this.chess.load(this.game.fen);
 
-    // This will only work if we are black move the pawn at b2 to b3.
-    // Otherwise we will lose.
-    // Your job is to code SOMETHING to parse the FEN string in some way to determine a valid move, in SAN format.
-    return 'b3';
+    this.printChessInfo('my');
+    this.printedEnemyMoves = false; // reset for next
+
+    const moves = this.chess.moves();
+    const randomMove = moves[Math.floor(Math.random() * moves.length)];
+
+    return randomMove;
+  }
+
+  printChessInfo(owner) {
+    console.log(`>> Start of ${owner} turn: ${Math.floor(this.game.history.length / 2)}`);
+    console.log(this.chess.ascii());
+
+    const moves = this.chess.moves();
+
+    console.log(`   ${owner} valid moves in SAN:`);
+    for (const move of moves) {
+      console.log(`   - ${move}`);
+    }
+    console.log("----------\n");
   }
 }
 
