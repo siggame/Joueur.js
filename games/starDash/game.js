ab099eb7ffd3511a961a7fe783ff087c57ee40e2
Strahm, Jeffrey S.(S&T-Student) <jssfg8@mst.edu>
2019-02-12 22:39:35 -0600

    Adding StarDash

---
+++ games/starDash/game.js
@@ -0,0 +1,352 @@
+// Game: Collect of the most of the rarest mineral orbiting aroung the sun and outcompete your competetor.
+
+// DO NOT MODIFY THIS FILE
+// Never try to directly create an instance of this class, or modify its member variables.
+// Instead, you should only be reading its variables and calling its functions.
+
+const client = require(`${__basedir}/joueur/client`);
+const BaseGame = require(`${__basedir}/joueur/baseGame`);
+
+//<<-- Creer-Merge: requires -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+// any additional requires you want can be required here safely between creer runs
+//<<-- /Creer-Merge: requires -->>
+
+/**
+ * Collect of the most of the rarest mineral orbiting aroung the sun and outcompete your competetor.
+ * @extends BaseGame
+ * @memberof StarDash
+ */
+class Game extends BaseGame {
+  /**
+   * Initializes a Game with basic logic as provided by the Creer code generator.
+   * 
+   * Never use this directly. It is for internal Joueur use.
+   */
+  constructor(...args) {
+    super(...args);
+
+
+    // The following values should get overridden when delta states are merged, but we set them here as a reference for you to see what variables this class has.
+
+    /**
+     * The name of the game.
+     * @type {string}
+     */
+    this.name = 'StarDash';
+
+    // default values for private member values
+    this.bodies = [];
+    this.currentPlayer = null;
+    this.currentTurn = 0;
+    this.dashDistance = 0;
+    this.gameObjects = {};
+    this.jobs = [];
+    this.maxAsteroid = 0;
+    this.maxTurns = 0;
+    this.minAsteroid = 0;
+    this.oreRarity1 = 0;
+    this.oreRarity2 = 0;
+    this.oreRarity3 = 0;
+    this.planetRechargeRate = 0;
+    this.players = [];
+    this.regenerateRate = 0;
+    this.session = '';
+    this.sizeX = 0;
+    this.sizeY = 0;
+    this.timeAddedPerTurn = 0;
+    this.units = [];
+
+    //<<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+    // any additional init logic you want can go here
+    //<<-- /Creer-Merge: init -->>
+  }
+
+
+  // Member variables
+
+  /**
+   * All the celestial bodies in the game.
+   *
+   * @type {Array.<StarDash.Body>}
+   */
+  get bodies() {
+    return client.gameManager.getMemberValue(this, 'bodies');
+  }
+
+  set bodies(value) {
+    client.gameManager.setMemberValue(this, 'bodies', value);
+  }
+
+
+  /**
+   * The player whose turn it is currently. That player can send commands. Other players cannot.
+   *
+   * @type {StarDash.Player}
+   */
+  get currentPlayer() {
+    return client.gameManager.getMemberValue(this, 'currentPlayer');
+  }
+
+  set currentPlayer(value) {
+    client.gameManager.setMemberValue(this, 'currentPlayer', value);
+  }
+
+
+  /**
+   * The current turn number, starting at 0 for the first player's turn.
+   *
+   * @type {number}
+   */
+  get currentTurn() {
+    return client.gameManager.getMemberValue(this, 'currentTurn');
+  }
+
+  set currentTurn(value) {
+    client.gameManager.setMemberValue(this, 'currentTurn', value);
+  }
+
+
+  /**
+   * The distance traveled each turn by dashing.
+   *
+   * @type {number}
+   */
+  get dashDistance() {
+    return client.gameManager.getMemberValue(this, 'dashDistance');
+  }
+
+  set dashDistance(value) {
+    client.gameManager.setMemberValue(this, 'dashDistance', value);
+  }
+
+
+  /**
+   * A mapping of every game object's ID to the actual game object. Primarily used by the server and client to easily refer to the game objects via ID.
+   *
+   * @type {Object.<string, StarDash.GameObject>}
+   */
+  get gameObjects() {
+    return client.gameManager.getMemberValue(this, 'gameObjects');
+  }
+
+  set gameObjects(value) {
+    client.gameManager.setMemberValue(this, 'gameObjects', value);
+  }
+
+
+  /**
+   * A list of all jobs. first item is corvette, second is missleboat, third is martyr, fourth is transport, and fifth is miner.
+   *
+   * @type {Array.<StarDash.Job>}
+   */
+  get jobs() {
+    return client.gameManager.getMemberValue(this, 'jobs');
+  }
+
+  set jobs(value) {
+    client.gameManager.setMemberValue(this, 'jobs', value);
+  }
+
+
+  /**
+   * The highest amount of material, barring rarity, that can be in a asteroid.
+   *
+   * @type {number}
+   */
+  get maxAsteroid() {
+    return client.gameManager.getMemberValue(this, 'maxAsteroid');
+  }
+
+  set maxAsteroid(value) {
+    client.gameManager.setMemberValue(this, 'maxAsteroid', value);
+  }
+
+
+  /**
+   * The maximum number of turns before the game will automatically end.
+   *
+   * @type {number}
+   */
+  get maxTurns() {
+    return client.gameManager.getMemberValue(this, 'maxTurns');
+  }
+
+  set maxTurns(value) {
+    client.gameManager.setMemberValue(this, 'maxTurns', value);
+  }
+
+
+  /**
+   * The smallest amount of material, barring rarity, that can be in a asteroid.
+   *
+   * @type {number}
+   */
+  get minAsteroid() {
+    return client.gameManager.getMemberValue(this, 'minAsteroid');
+  }
+
+  set minAsteroid(value) {
+    client.gameManager.setMemberValue(this, 'minAsteroid', value);
+  }
+
+
+  /**
+   * The rarity modifier of the most common ore. This controls how much spawns.
+   *
+   * @type {number}
+   */
+  get oreRarity1() {
+    return client.gameManager.getMemberValue(this, 'oreRarity1');
+  }
+
+  set oreRarity1(value) {
+    client.gameManager.setMemberValue(this, 'oreRarity1', value);
+  }
+
+
+  /**
+   * The rarity modifier of the second rarest ore. This controls how much spawns.
+   *
+   * @type {number}
+   */
+  get oreRarity2() {
+    return client.gameManager.getMemberValue(this, 'oreRarity2');
+  }
+
+  set oreRarity2(value) {
+    client.gameManager.setMemberValue(this, 'oreRarity2', value);
+  }
+
+
+  /**
+   * The rarity modifier of the rarest ore. This controls how much spawns.
+   *
+   * @type {number}
+   */
+  get oreRarity3() {
+    return client.gameManager.getMemberValue(this, 'oreRarity3');
+  }
+
+  set oreRarity3(value) {
+    client.gameManager.setMemberValue(this, 'oreRarity3', value);
+  }
+
+
+  /**
+   * The amount of energy the planets restore each round.
+   *
+   * @type {number}
+   */
+  get planetRechargeRate() {
+    return client.gameManager.getMemberValue(this, 'planetRechargeRate');
+  }
+
+  set planetRechargeRate(value) {
+    client.gameManager.setMemberValue(this, 'planetRechargeRate', value);
+  }
+
+
+  /**
+   * List of all the players in the game.
+   *
+   * @type {Array.<StarDash.Player>}
+   */
+  get players() {
+    return client.gameManager.getMemberValue(this, 'players');
+  }
+
+  set players(value) {
+    client.gameManager.setMemberValue(this, 'players', value);
+  }
+
+
+  /**
+   * The regeneration rate of asteroids.
+   *
+   * @type {number}
+   */
+  get regenerateRate() {
+    return client.gameManager.getMemberValue(this, 'regenerateRate');
+  }
+
+  set regenerateRate(value) {
+    client.gameManager.setMemberValue(this, 'regenerateRate', value);
+  }
+
+
+  /**
+   * A unique identifier for the game instance that is being played.
+   *
+   * @type {string}
+   */
+  get session() {
+    return client.gameManager.getMemberValue(this, 'session');
+  }
+
+  set session(value) {
+    client.gameManager.setMemberValue(this, 'session', value);
+  }
+
+
+  /**
+   * The size of the map in the X direction.
+   *
+   * @type {number}
+   */
+  get sizeX() {
+    return client.gameManager.getMemberValue(this, 'sizeX');
+  }
+
+  set sizeX(value) {
+    client.gameManager.setMemberValue(this, 'sizeX', value);
+  }
+
+
+  /**
+   * The size of the map in the Y direction.
+   *
+   * @type {number}
+   */
+  get sizeY() {
+    return client.gameManager.getMemberValue(this, 'sizeY');
+  }
+
+  set sizeY(value) {
+    client.gameManager.setMemberValue(this, 'sizeY', value);
+  }
+
+
+  /**
+   * The amount of time (in nano-seconds) added after each player performs a turn.
+   *
+   * @type {number}
+   */
+  get timeAddedPerTurn() {
+    return client.gameManager.getMemberValue(this, 'timeAddedPerTurn');
+  }
+
+  set timeAddedPerTurn(value) {
+    client.gameManager.setMemberValue(this, 'timeAddedPerTurn', value);
+  }
+
+
+  /**
+   * Every Unit in the game.
+   *
+   * @type {Array.<StarDash.Unit>}
+   */
+  get units() {
+    return client.gameManager.getMemberValue(this, 'units');
+  }
+
+  set units(value) {
+    client.gameManager.setMemberValue(this, 'units', value);
+  }
+
+
+  //<<-- Creer-Merge: functions -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+  // any additional functions you want to add to this class can be preserved here
+  //<<-- /Creer-Merge: functions -->>
+}
+
+module.exports = Game;
---
+++ games/starDash/unit.js
@@ -0,0 +1,329 @@
+// Unit: A unit in the game. May be a corvette, missleboat, martyr, transport, miner.
+
+// DO NOT MODIFY THIS FILE
+// Never try to directly create an instance of this class, or modify its member variables.
+// Instead, you should only be reading its variables and calling its functions.
+
+const client = require(`${__basedir}/joueur/client`);
+const GameObject = require('./gameObject');
+
+//<<-- Creer-Merge: requires -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+// any additional requires you want can be required here safely between creer runs
+//<<-- /Creer-Merge: requires -->>
+
+/**
+ * A unit in the game. May be a corvette, missleboat, martyr, transport, miner.
+ * @extends StarDash.GameObject
+ * @memberof StarDash
+ */
+class Unit extends GameObject {
+  /**
+   * Initializes a Unit with basic logic as provided by the Creer code generator.
+   * 
+   * Never use this directly. It is for internal Joueur use.
+   */
+  constructor(...args) {
+    super(...args);
+
+
+    // The following values should get overridden when delta states are merged, but we set them here as a reference for you to see what variables this class has.
+
+    // default values for private member values
+    this.acted = false;
+    this.energy = 0;
+    this.genarium = 0;
+    this.isDashing = false;
+    this.job = null;
+    this.legendarium = 0;
+    this.moves = 0;
+    this.mythicite = 0;
+    this.owner = null;
+    this.radius = 0;
+    this.rarium = 0;
+    this.x = 0;
+    this.y = 0;
+
+    //<<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+    // any additional init logic you want can go here
+    //<<-- /Creer-Merge: init -->>
+  }
+
+
+  // Member variables
+
+  /**
+   * Whether or not this Unit has performed its action this turn.
+   *
+   * @type {boolean}
+   */
+  get acted() {
+    return client.gameManager.getMemberValue(this, 'acted');
+  }
+
+  set acted(value) {
+    client.gameManager.setMemberValue(this, 'acted', value);
+  }
+
+
+  /**
+   * The remaining health of a unit.
+   *
+   * @type {number}
+   */
+  get energy() {
+    return client.gameManager.getMemberValue(this, 'energy');
+  }
+
+  set energy(value) {
+    client.gameManager.setMemberValue(this, 'energy', value);
+  }
+
+
+  /**
+   * The amount of Generium ore carried by this unit. (0 to job carry capacity - other carried items).
+   *
+   * @type {number}
+   */
+  get genarium() {
+    return client.gameManager.getMemberValue(this, 'genarium');
+  }
+
+  set genarium(value) {
+    client.gameManager.setMemberValue(this, 'genarium', value);
+  }
+
+
+  /**
+   * Tracks wheither or not the ship is dashing.
+   *
+   * @type {boolean}
+   */
+  get isDashing() {
+    return client.gameManager.getMemberValue(this, 'isDashing');
+  }
+
+  set isDashing(value) {
+    client.gameManager.setMemberValue(this, 'isDashing', value);
+  }
+
+
+  /**
+   * The Job this Unit has.
+   *
+   * @type {StarDash.Job}
+   */
+  get job() {
+    return client.gameManager.getMemberValue(this, 'job');
+  }
+
+  set job(value) {
+    client.gameManager.setMemberValue(this, 'job', value);
+  }
+
+
+  /**
+   * The amount of Legendarium ore carried by this unit. (0 to job carry capacity - other carried items).
+   *
+   * @type {number}
+   */
+  get legendarium() {
+    return client.gameManager.getMemberValue(this, 'legendarium');
+  }
+
+  set legendarium(value) {
+    client.gameManager.setMemberValue(this, 'legendarium', value);
+  }
+
+
+  /**
+   * The distance this unit can still move.
+   *
+   * @type {number}
+   */
+  get moves() {
+    return client.gameManager.getMemberValue(this, 'moves');
+  }
+
+  set moves(value) {
+    client.gameManager.setMemberValue(this, 'moves', value);
+  }
+
+
+  /**
+   * The amount of Mythicite carried by this unit. (0 to job carry capacity - other carried items).
+   *
+   * @type {number}
+   */
+  get mythicite() {
+    return client.gameManager.getMemberValue(this, 'mythicite');
+  }
+
+  set mythicite(value) {
+    client.gameManager.setMemberValue(this, 'mythicite', value);
+  }
+
+
+  /**
+   * The Player that owns and can control this Unit.
+   *
+   * @type {StarDash.Player}
+   */
+  get owner() {
+    return client.gameManager.getMemberValue(this, 'owner');
+  }
+
+  set owner(value) {
+    client.gameManager.setMemberValue(this, 'owner', value);
+  }
+
+
+  /**
+   * The radius of the circle this unit occupies.
+   *
+   * @type {number}
+   */
+  get radius() {
+    return client.gameManager.getMemberValue(this, 'radius');
+  }
+
+  set radius(value) {
+    client.gameManager.setMemberValue(this, 'radius', value);
+  }
+
+
+  /**
+   * The amount of Rarium carried by this unit. (0 to job carry capacity - other carried items).
+   *
+   * @type {number}
+   */
+  get rarium() {
+    return client.gameManager.getMemberValue(this, 'rarium');
+  }
+
+  set rarium(value) {
+    client.gameManager.setMemberValue(this, 'rarium', value);
+  }
+
+
+  /**
+   * The x value this unit is on.
+   *
+   * @type {number}
+   */
+  get x() {
+    return client.gameManager.getMemberValue(this, 'x');
+  }
+
+  set x(value) {
+    client.gameManager.setMemberValue(this, 'x', value);
+  }
+
+
+  /**
+   * The y value this unit is on.
+   *
+   * @type {number}
+   */
+  get y() {
+    return client.gameManager.getMemberValue(this, 'y');
+  }
+
+  set y(value) {
+    client.gameManager.setMemberValue(this, 'y', value);
+  }
+
+
+
+  /**
+   * Attacks the specified unit.
+   *
+   * @param {StarDash.Unit} enemy - The Unit being attacked.
+   * @returns {boolean} - True if successfully attacked, false otherwise.
+   */
+  attack(enemy) {
+    return client.runOnServer(this, 'attack', {
+      enemy: enemy,
+    });
+  }
+
+
+  /**
+   * allows a miner to mine a asteroid
+   *
+   * @param {StarDash.Body} body - The object to be mined.
+   * @returns {boolean} - True if successfully acted, false otherwise.
+   */
+  mine(body) {
+    return client.runOnServer(this, 'mine', {
+      body: body,
+    });
+  }
+
+
+  /**
+   * Moves this Unit from its current location to the new location specified.
+   *
+   * @param {number} x - The x value of the destination's coordinates.
+   * @param {number} y - The y value of the destination's coordinates.
+   * @returns {boolean} - True if it moved, false otherwise.
+   */
+  move(x, y) {
+    return client.runOnServer(this, 'move', {
+      x: x,
+      y: y,
+    });
+  }
+
+
+  /**
+   * tells you if your ship can be at that location.
+   *
+   * @param {number} x - The x position of the location you wish to check.
+   * @param {number} y - The y position of the location you wish to check.
+   * @returns {boolean} - True if pathable by this unit, false otherwise.
+   */
+  open(x, y) {
+    return client.runOnServer(this, 'open', {
+      x: x,
+      y: y,
+    });
+  }
+
+
+  /**
+   * tell you if there is a open path to that location.
+   *
+   * @param {number} x - The x position of the destination you wish to check to.
+   * @param {number} y - The y position of the destination you wish to check to.
+   * @returns {boolean} - True if pathable by this unit, false otherwise.
+   */
+  pathable(x, y) {
+    return client.runOnServer(this, 'pathable', {
+      x: x,
+      y: y,
+    });
+  }
+
+
+  /**
+   * Grab materials from a friendly unit. Doesn't use a action.
+   *
+   * @param {StarDash.Unit} unit - The unit you are grabbing the resources from.
+   * @param {number} amount - The amount of materials to you with to grab. Amounts <= 0 will pick up all the materials that the unit can.
+   * @param {string} material - The material the unit will pick up. 'resource1', 'resource2', or 'resource3'.
+   * @returns {boolean} - True if successfully taken, false otherwise.
+   */
+  transfer(unit, amount, material) {
+    return client.runOnServer(this, 'transfer', {
+      unit: unit,
+      amount: amount,
+      material: material,
+    });
+  }
+
+  //<<-- Creer-Merge: functions -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+  // any additional functions you want to add to this class can be preserved here
+  //<<-- /Creer-Merge: functions -->>
+}
+
+module.exports = Unit;
