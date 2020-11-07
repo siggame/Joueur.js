3ea27eb57f6242159c6d4332d7c459b3f964ffc6
Jonathan Henderson <jkhenderson999@gmail.com>
2020-11-07 08:43:04 -0600

    Update timeAddedPerTurn

--- games/coreminer/gameManager.js
+++ games/coreminer/gameManager.js
@@ -11,7 +11,7 @@ const GameManager = require(`${__basedir}/joueur/gameManager`);
 
 class CoreminerGameManager extends GameManager {}
 
-CoreminerGameManager.gameVersion = 'a4592bb5acb0415146605769f439a09baf3768f41cdb3c7ad9dc971f42c4d96e';
+CoreminerGameManager.gameVersion = 'b559778acd8e4c689b8d028ca6cc154714ce79c39b09cd6d171b50faf88ef747';
 
 CoreminerGameManager.prototype._gameObjectClasses = {
   Bomb: require('./bomb'),
